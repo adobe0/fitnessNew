1f7cf195e23e38b1de20af4a368c99fc1abfdd4c
adobe0 <53244348+adobe0@users.noreply.github.com>
2023-08-21 22:41:57 +0800

    Update landing page.kt

--- app/src/main/java/com/example/fitnesstracker/MainPages/landing page.kt
+++ app/src/main/java/com/example/fitnesstracker/MainPages/landing page.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.unit.sp
 fun LandingPage() {
     Column {
         Text(
-            text = "Fuck you vansh",
+            text = "hello",
             fontSize = 100.sp
         )
     }
