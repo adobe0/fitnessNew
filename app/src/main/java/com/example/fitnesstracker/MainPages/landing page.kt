aa817d41df2ca83c6fea170d47e3ecdabc9bc6f5
aadit <aadit.kanodia@gmail.com>
2023-09-07 18:04:33 +0800

    database start

--- app/src/main/java/com/example/fitnesstracker/MainPages/landing page.kt
+++ app/src/main/java/com/example/fitnesstracker/MainPages/landing page.kt
@@ -12,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.fitnesstracker.R
+import com.example.fitnesstracker.assets.RecipePreviewCard
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,9 +34,26 @@ fun LandPage() {
         ) {
             Text(text = "Add food")
         }
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(text = "Reccomended:", Modifier.padding(start = 5.dp))
+        RecipePreviewCard(
+            imageUrl = R.drawable.sample_card, // Hypothetical drawable ID for a recipe image
+            title = "Delicious Pasta",
+            description = "Delve into the rich and creamy flavors of this delightful pasta dish. Prepared with fresh basil, oregano, and a sprinkle of love, our recipe brings forth the authentic taste of Italian cuisine. The sun-dried tomatoes and parmesan shavings not only enhance the taste but also present an aesthetic appeal. Paired best with a glass of white wine.",
+            icons = listOf(R.drawable.noun_vegan_3029210, R.drawable.noun_stopwatch_5062298), // Hypothetical drawable IDs for icons
+            onCardClick = {}
+        )
 
-    }
-}
+    Text(text = "Your Plan", modifier = Modifier.padding(start = 5.dp,top = 15.dp))
+        RecipePreviewCard(
+            imageUrl = R.drawable.sample_card, // Hypothetical drawable ID for a recipe image
+            title = "UnDelicious Pasta",
+            description = "Delve into the rich and creamy flavors of this delightful pasta dish. Prepared with fresh basil, oregano, and a sprinkle of love, our recipe brings forth the authentic taste of Italian cuisine. The sun-dried tomatoes and parmesan shavings not only enhance the taste but also present an aesthetic appeal. Paired best with a glass of white wine.",
+            icons = listOf(R.drawable.noun_vegan_3029210, R.drawable.noun_stopwatch_5062298), // Hypothetical drawable IDs for icons
+            onCardClick = {}
+        )
+
+}}
 
 @Preview(showSystemUi = true)
 @Composable
