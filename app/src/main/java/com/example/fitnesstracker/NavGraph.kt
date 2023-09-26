9cba4e9ec75e2cc2978ffc29ac13c374386b564e
aadit <kanod95637@gapps.uwcsea.edu.sg>
2023-09-26 20:00:38 +0800

    nov host implimentation (Log-in: Pages)

--- app/src/main/java/com/example/fitnesstracker/NavGraph.kt
+++ app/src/main/java/com/example/fitnesstracker/NavGraph.kt
@@ -4,23 +4,50 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.fitnesstracker.MainPages.ExplorePage
-import com.example.fitnesstracker.screen.*
 import com.example.fitnesstracker.MainPages.LandPage
+import com.example.fitnesstracker.logInUserVarification.EmailPage
+import com.example.fitnesstracker.logInUserVarification.ForgotPasswordSecurity
+import com.example.fitnesstracker.logInUserVarification.PasswordReset
+import com.example.fitnesstracker.logInUserVarification.SecurityQuestionPage
+import com.example.fitnesstracker.logInUserVarification.SignUpPage
 
 @Composable
 fun SetupNavGraph(navController: NavHostController){
     NavHost(navController = navController,
-        startDestination = screen.landingPage.route
+        startDestination = screen.login.route
     ){
         composable(
             route = screen.landingPage.route){
-            LandPage(navController = rememberNavController())
+            LandPage(navController = navController)
         }
         composable(
             route = screen.explore.route){
             ExplorePage()
+        }
+        composable(
+            route = screen.login.route){
+            EmailPage()
+        }
+        composable(
+            route = screen.forgotPass1.route){
+            ForgotPasswordSecurity()
+        }
+        composable(
+            route = screen.forgotpass2.route){
+            PasswordReset()
+        }
+        composable(
+            route = screen.forgotPass1.route){
+            ForgotPasswordSecurity()
+        }
+        composable(
+            route = screen.newUser.route){
+            SignUpPage()
+        }
+        composable(
+            route = screen.NUSecQ.route){
+            SecurityQuestionPage()
         }
     }
 }
--- app/src/main/java/com/example/fitnesstracker/assets/burger menue.kt
+++ app/src/main/java/com/example/fitnesstracker/assets/burger menue.kt
@@ -1,84 +1,53 @@
-/*package com.example.fitnesstracker.assets
 
-import androidx.compose.foundation.background
+package com.example.fitnesstracker.logInUserVarification
+
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.fitnesstracker.R
-import com.example.fitnesstracker.assets.PlanNugget
-import com.example.fitnesstracker.assets.RecipePreviewCard
-
-
-@Composable
-fun AppNavigator() {
-    val navController = rememberNavController()
-    val openDrawer = rememberUpdatedState(false)
-    val scaffoldState = rememberScaffoldState()
-
-    Scaffold(
-        scaffoldState = scaffoldState,
-        drawerContent = {
-            // Drawer content
-            Drawer(
-                onDestinationClicked = { route ->
-                    navController.navigate(route) {
-                        popUpTo(navController.graph.startDestinationId)
-                        launchSingleTop = true
-                    }
-                    scaffoldState.drawerState.close()
-                }
-            )
-        }
-    ) {
-        NavHost(navController = navController, startDestination = MainDestinations.NEW_USER_ROUTE) {
-            composable(MainDestinations.NEW_USER_ROUTE) { NewUserPage() }
-            composable(MainDestinations.LOGIN_ROUTE) { LoginPage() }
-            // ... other composable destinations
-        }
-    }
-}
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.fitnesstracker.screen
 
 @Composable
-fun Drawer(onDestinationClicked: (String) -> Unit) {
-    Column {
-        DrawerButton("New User", MainDestinations.NEW_USER_ROUTE, onDestinationClicked)
-        DrawerButton("Login", MainDestinations.LOGIN_ROUTE, onDestinationClicked)
-        // ... other DrawerButtons
-    }
-}
-
-@Composable
-fun DrawerButton(
-    text: String,
-    route: String,
-    onDestinationClicked: (String) -> Unit
-) {
-    Text(
-        text = text,
+fun menue(navController: NavHostController) {
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onDestinationClicked(route) }
+            .fillMaxSize()
             .padding(16.dp)
-    )
+    ) {
+        Text(
+            text = "Landing Page",
+            modifier = Modifier
+                .padding(8.dp)
+                .clickable {
+                    navController.navigate(screen.landingPage.route)
+                },
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Explore Page",
+            modifier = Modifier
+                .padding(8.dp)
+                .clickable {
+                    navController.navigate(screen.explore.route)
+                },
+            fontSize = 20.sp
+        )
+
+    }
 }
-*/
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun showMe(){
+    menue(navController = rememberNavController())
+}
